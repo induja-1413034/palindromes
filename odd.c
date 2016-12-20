ea20918794b349595283f1ce9ee0d3159892d506
induja-1413034 <induja26.1997@gmail.com>
2016-12-20 15:50:56 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\nenter the no:");
+scanf("%d",&a);
+if(a%2==0)
+printf("it is even no");
+else
+printf("It is odd no");
+getch();
+}
