42d3f201bd1c925c588da9f5be5b1424c55996ae
induja-1413034 <induja26.1997@gmail.com>
2017-07-14 15:56:49 +0530

    Add files via upload

---
+++ insertionsortingalg.java
@@ -0,0 +1,18 @@
+package GuviLogic;
+
+import java.util.TreeSet;
+
+public class insertionsortingalg {
+	public static void main(String[] args){
+		
+		TreeSet<Integer> a=new TreeSet<Integer>();
+		TreeSet<Integer> b=new TreeSet<Integer>();
+		a.add(35);
+		a.add(45);
+		a.add(15);
+		a.add(5);
+		System.out.println("The sorted order in the insertion sort  is"+a);
+			
+		}
+
+}
