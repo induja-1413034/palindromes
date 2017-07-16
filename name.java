9e603068a7e0ee0fc95f329c9ead59c249309cb9
induja-1413034 <induja26.1997@gmail.com>
2017-07-16 11:44:46 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,12 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class name {
+public static void main(String[] args){
+	String a;
+	Scanner s=new Scanner(System.in);
+	a=s.next();
+	System.out.print(a+","+a.substring(0,1));
+}
+}
