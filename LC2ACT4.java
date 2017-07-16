c7a8d635e4a1846226d2992f4868e2efc949afb0
induja-1413034 <induja26.1997@gmail.com>
2017-07-16 11:50:19 +0530

    Add files via upload

---
+++ LC2ACT4.java
@@ -0,0 +1,13 @@
+package LC2;
+
+import java.util.Scanner;
+//BINARY TO DECIMAL
+public class LC2ACT4 {
+	public static void main(String[] args){
+		String a;
+		Scanner s=new Scanner(System.in);
+		a=s.next();
+		System.out.println(Integer.parseInt(a,2));
+	}
+
+}
