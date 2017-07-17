778ebc0a4b5c5e1b1a69317b3ff4e3ee5a1f782c
induja-1413034 <induja26.1997@gmail.com>
2017-07-17 09:25:08 +0530

    Add files via upload

---
+++ arrayindexequals.java
@@ -0,0 +1,20 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class arrayindexequals {
+public static void main(String[] args){
+	Scanner s=new Scanner(System.in);
+	int n;
+	n=s.nextInt();
+	int[] a=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<n;i++){
+		if(i==a[i]){
+			System.out.println(i);
+		}
+	}
+}
+}
