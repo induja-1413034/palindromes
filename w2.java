4a107539d25651a4315c7cb8ba43632de1cb984a
induja-1413034 <induja26.1997@gmail.com>
2017-07-17 14:34:49 +0530

    Add files via upload

---
+++ w2.java
@@ -0,0 +1,19 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class w2 {
+	public static void main(String[] args){
+		String a;
+		Scanner s=new Scanner(System.in);
+		a=s.next();
+
+		if(a.equalsIgnoreCase("monday")||a.equalsIgnoreCase("tuesday")||a.equalsIgnoreCase("wednesday")||a.equalsIgnoreCase("thursday")||a.equalsIgnoreCase("friday")){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+		}
+		}
+
+}
