780b8ba012c0b0545820c55af9b6b7bd1fda67cc
induja-1413034 <induja26.1997@gmail.com>
2017-07-22 12:17:34 +0530

    Add files via upload

---
+++ rep2.java
@@ -0,0 +1,28 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class rep2 {
+	public static void main(String[] args){
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<a.length;i++){
+			for(int j=i+1;j<a.length;j++){
+				if(a[i]==a[j]){
+						a[i]=0;
+						a[j]=0;
+				}
+			}
+		}
+		for(int i=0;i<n;i++){
+			if(a[i]!=0){
+				System.out.println(a[i]);
+			}
+		}
+	}
+}
